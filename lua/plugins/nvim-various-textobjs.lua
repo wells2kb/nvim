e49d211da557846032660f40cd451317d336903d
Kaleb Wells <kaleb.wells@msasafety.com>
2024-11-27 14:07:08 -0500

    remap text-objects to make more sense

--- lua/plugins/nvim-various-textobjs.lua
+++ lua/plugins/nvim-various-textobjs.lua
@@ -4,7 +4,15 @@ return {
   config = function ()
     require("various-textobjs").setup({
       useDefaultKeymaps = true,
-      disabledKeymaps = {"r"}
+      disabledKeymaps = {"r", "aS", "iS", "a_", "i_", "al", "il"}
     })
   end,
+  keys = {
+    vim.keymap.set({ "o", "x" }, "a_", '<cmd>lua require("various-textobjs").subword("outer")<CR>'),
+    vim.keymap.set({ "o", "x" }, "i_", '<cmd>lua require("various-textobjs").subword("inner")<CR>'),
+    vim.keymap.set({ "o", "x" }, "a-", '<cmd>lua require("various-textobjs").subword("outer")<CR>'),
+    vim.keymap.set({ "o", "x" }, "i-", '<cmd>lua require("various-textobjs").subword("inner")<CR>'),
+    vim.keymap.set({ "o", "x" }, "al", '<cmd>lua require("various-textobjs").lineCharacterwise("outer")<CR>'),
+    vim.keymap.set({ "o", "x" }, "il", '<cmd>lua require("various-textobjs").lineCharacterwise("inner")<CR>')
+  }
 }
