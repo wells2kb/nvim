e72da70b5ee4875ba1ceb37b0fab5e9bffd8c628
Kaleb Wells <wells2kb@mail.uc.edu>
2025-04-02 22:27:11 -0400

    fix paragraph start navigation

--- lua/remap.lua
+++ lua/remap.lua
@@ -1,9 +1,9 @@
 vim.g.mapleader = " "
 
--- Ctrl+C == Esc
+-- Ctrl+C == esc
 vim.keymap.set("n", "<C-c>", "<ESC>")
 
--- Paste & Maintain Register
+-- Paste & maintain register
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
 -- Seriously, quit terminal mode
@@ -24,49 +24,49 @@ vim.keymap.set("v", "<leader>lf",
   [[:s/\%V/\r/]]
 )
 
--- No Highlight Search
+-- No highlight search
 vim.keymap.set("n", "<leader>n", "<CMD>nohl<CR>")
 
--- Delete Buffer, Keep Split
+-- Delete buffer, keep split
 vim.keymap.set("n", "<leader>q", "<CMD>bp|bd#<CR>")
 
--- Cursor to Middle for PgUp/PgDown
+-- Cursor to middle for pgup/pgdown
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
--- Cursor to Middle for Search Next/Previous
+-- Cursor to middle for search next/previous
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
--- Stay in Indent Mode
+-- Stay in indent mode
 vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
 
--- Jump Splits with Arrows
+-- Jump splits with arrows
 vim.keymap.set("n", "<S-Left>",  "<C-w><Left>")
 vim.keymap.set("n", "<S-Down>",  "<C-w><Down>")
 vim.keymap.set("n", "<S-Up>",    "<C-w><Up>")
 vim.keymap.set("n", "<S-Right>", "<C-w><Right>")
 
--- Resize Splits with Arrows
+-- Resize splits with arrows
 vim.keymap.set("n", "<C-Left>",  ":vertical resize -2<CR>")
 vim.keymap.set("n", "<C-Down>",  ":resize -2<CR>")
 vim.keymap.set("n", "<C-Up>",    ":resize +2<CR>")
 vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>")
 
--- Move Splits with Arrows
+-- Move splits with arrows
 vim.keymap.set("n", "<A-Left>",  "<C-w><S-h>")
 vim.keymap.set("n", "<A-Down>",  "<C-w><S-j>")
 vim.keymap.set("n", "<A-Up>",    "<C-w><S-k>")
 vim.keymap.set("n", "<A-Right>", "<C-w><S-l>")
 
--- Go to File At Line Number
+-- Go to file at line number
 vim.keymap.set("n", "gf",  "gF")
 
--- Go to File At Line Number Under Cursor in Vertical Split
+-- Go to file at line number under cursor in vertical split
 vim.keymap.set("n", "gF",  "<C-w>v<C-w>wgF")
 
--- Surround Visual Selection
+-- Surround visual selection
 vim.keymap.set("v", "s ",  "c <C-r>\" <C-c>")
 vim.keymap.set("v", "s(",  "c(<C-r>\")<C-c>")
 vim.keymap.set("v", "s-",  "c-<C-r>\"-<C-c>")
@@ -78,14 +78,25 @@ vim.keymap.set("v", "s_",  "c_<C-r>\"_<C-c>")
 vim.keymap.set("v", "s`",  "c`<C-r>\"`<C-c>")
 vim.keymap.set("v", "s{",  "c{<C-r>\"}<C-c>")
 
--- Jump to Paragraph Start Backwards
+-- Don't grab the newline in visual
+vim.keymap.set("v", "<End>",  "<End><Left>")
+
+-- Jump to paragraph start backwards
 local jump_to_paragraph_start_backwards = function()
-  if vim.fn.line(".") == (vim.fn.line("'{") + 1) then
-    vim.fn.cursor(vim.fn.line("'{") - 1, 1)
+  local paragraph_start = vim.fn.line("'{")
+  if vim.fn.line(".") == (paragraph_start + 1) then
+    if paragraph_start == 1 then
+      vim.fn.cursor(1, 1)
+      return
+    else
+      vim.fn.cursor(paragraph_start - 1, 1)
+    end
   end
 
-  local paragraph_start = vim.fn.line("'{")
-  if paragraph_start == 1 then
+  paragraph_start = vim.fn.line("'{")
+  if paragraph_start == 1 and (
+    vim.fn.getline("1") ~= "" or vim.fn.getline("1,2") == ""
+  ) then
     vim.fn.cursor(1, 1)
   else
     vim.fn.cursor(paragraph_start + 1, 1)
@@ -95,19 +106,24 @@ local jump_to_paragraph_start_backwards = function()
 end
 vim.keymap.set({"n", "v"}, "(", jump_to_paragraph_start_backwards)
 
--- TODO doesn't work outside of a paragraph
--- Jump to Paragraph Start Forwards
+-- Jump to paragraph start forwards
 local jump_to_paragraph_start_forwards = function()
-  if vim.fn.line("'}") ~= vim.fn.line("$") then
-    vim.fn.cursor(vim.fn.line("'}") + 1, 1)
-    vim.fn.cursor(vim.fn.line("'{") + 1, 1)
+  if vim.fn.line("'{") == 1 then
+    vim.fn.cursor(vim.fn.line("')"), 1)
+  elseif vim.fn.line("'}") ~= vim.fn.line("$") then
+    vim.fn.cursor(vim.fn.line("'{"), 1)
+    vim.fn.cursor(vim.fn.line("'}"), 1)
+    vim.fn.cursor(vim.fn.line("')"), 1)
     vim.fn.feedkeys("^")
+  else
+    vim.fn.cursor(vim.fn.line("'}"), 1)
+    vim.fn.feedkeys("$")
   end
 end
 vim.keymap.set({"n", "v"}, ")", jump_to_paragraph_start_forwards)
 
 -- TODO doesn't work outside of a paragraph
--- Jump to Paragraph End Backwards
+-- Jump to paragraph end backwards
 local jump_to_paragraph_end_backwards = function()
   if vim.fn.line("'{") ~= vim.fn.line("1") then
     vim.fn.cursor(vim.fn.line("'{") - 1, 1)
@@ -117,7 +133,7 @@ local jump_to_paragraph_end_backwards = function()
 end
 vim.keymap.set({"n", "v"}, "[)", jump_to_paragraph_end_backwards)
 
--- Jump to Paragraph End Forwards
+-- Jump to paragraph end forwards
 local jump_to_paragraph_end_forwards = function()
   if vim.fn.line(".") == (vim.fn.line("'}") - 1) then
     vim.fn.cursor(vim.fn.line(".") + 1, 1)
@@ -185,6 +201,3 @@ local jump_to_indentation_start = function()
   end
 end
 vim.keymap.set({"n", "v"}, "[i", jump_to_indentation_start)
-
--- Don't Grab the Newline in Visual
-vim.keymap.set("v", "<End>",  "<End><Left>")
